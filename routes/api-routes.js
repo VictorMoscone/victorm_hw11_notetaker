8ec731acdf135fbd2660a1faa49e9eb669f2c572
Victor Moscone <victormoscone@gmail.com>
2020-12-11 20:20:47 -0800

    Server: API Route to the notes db.json file.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -6,4 +6,11 @@ router.get("/test", (req, res) => {
     return res.json({msg: "Successfully added."});
 });
 
+router.get("/notes", (req, res) => {
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+        if(err) throw err;
+        res.json(JSON.parse(data));
+    });
+});
+
 module.exports = router;
